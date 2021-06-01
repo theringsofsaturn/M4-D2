af662753076b30228260e64c7e8cb26b3dedb85a
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-06-01 17:03:51 +0200

    commits

---
+++ src/components/Latest.jsx
@@ -0,0 +1,19 @@
+const BookCategories = ['fantasty', 'history', 'romance', 'scifi', 'horror']
+const books = {
+  fantasty,
+  history,
+  romance,
+  scifi,
+  horror,
+}
+
+class LatestRelease extends React.Component{
+    state = {
+        data: books.fantasty,
+        categorySelected: 'fantasy',
+      }
+
+      render(){
+          
+      }
+}
